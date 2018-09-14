925a9ebc0e58df1a1ca0ee34adcf5d2c7139e965
benfarice <imzoughene@outlook.com>
2018-09-14 15:54:44 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'imzoughene youssef ben mohamed ben ahmed ben farice a Muslim boy';
 }
